d44bea4b62abc19baafef45dcb6798463b36d393
Shreyas521032 <149940538+Shreyas521032@users.noreply.github.com>
2023-11-05 20:53:22 +0530

    Delete src directory

--- src/error_handler.py
+++
@@ -1,5 +0,0 @@
-# error_handler.py
-
-def handle_error(error):
-    print(f"An error occurred: {error}")
-    # Add more specific error handling if needed
--- src/input_handler.py
+++
@@ -1,6 +0,0 @@
-# input_handler.py
-
-def get_user_location():
-    location = input("Enter a city name or ZIP code: ")
-    # Add validation logic if needed
-    return location
